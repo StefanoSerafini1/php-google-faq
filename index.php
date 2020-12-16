37d08669192a17c722f424d5f31332361312d4c6
StefanoSerafini <stefanoserafini7@gmail.com>
2020-12-16 15:44:36 +0100

    html navbar base

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FAQ google rifacimento</title>
+</head>
+<body>
+<header>
+        <div class="logo">
+            <img src=".img/google-logo.jpg" alt="logo Google">
+            <span>Privacy e termini</span>
+        </div>
+        <div class="navbar">
+            <ul class="list-navbar">
+                <li>
+                    <a href="#">Overview</a>
+                </li>
+                <li>
+                    <a href="#">Privacy Policy</a>
+                </li>
+                <li>
+                    <a href="#">Terms of Services</a>
+                </li>
+                <li>
+                    <a href="#">Technologies</a>
+                </li>
+                <li class="selected">
+                    <a href="#">FAQ</a>
+                </li>
+            </ul>
+            <span><img src=".img/avatar.png" alt=""></span>
+        </div>
+    </header>
+
+
+
+
+
+</body>
+</html>
