12010fb9710d136c377d9d36b5e6fe9f5d487d87
StefanoSerafini <stefanoserafini7@gmail.com>
2020-12-16 16:22:30 +0100

    html e css nav

--- index.php
+++ index.php
@@ -3,13 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet"> 
     <title>FAQ google rifacimento</title>
 </head>
 <body>
 <header>
-        <div class="logo">
-            <img src=".img/google-logo.jpg" alt="logo Google">
-            <span>Privacy e termini</span>
+        <div class="navtop">
+            <div class="logo">
+                <img src="img/google-logo.png" alt="logo Google">
+                <span>Privacy & Terms</span>
+            </div>
+            <div class="avatar">
+                <i class="fas fa-folder"></i>
+                <img src="img/avatar.png" alt="avatar">
+            </div>
         </div>
         <div class="navbar">
             <ul class="list-navbar">
@@ -29,9 +37,9 @@
                     <a href="#">FAQ</a>
                 </li>
             </ul>
-            <span><img src=".img/avatar.png" alt=""></span>
         </div>
     </header>
+    <hr>
 
 
 
